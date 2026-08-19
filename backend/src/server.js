0dc7e9d2110634c0dd5b93cc5605decfa6fcb723
rohitsinghh01 <singh.rohitraj122@gmail.com>
2026-08-19 23:16:28 +0530

    inngest added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,30 @@
 import express from "express";
+import path from "path";
+import cors from "cors";
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
 
 const app = express();
+const __dirname = path.resolve();
 
+app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
 });
+
+
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+
 
 const startServer = async () => {
   try {
